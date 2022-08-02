a2a4abddc297de5d21feb7b461d31ee147c1d62a
Rasim Aghayev <rasimaqayev@gmail.com>
2022-08-03 02:38:32 +0400

    Mini update and Refactoring

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,71 +1,64 @@
-import {Col, Container, Row} from "react-bootstrap";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css"
-import meter1 from '../assets/img/meter1.svg'
-import meter2 from '../assets/img/meter2.svg'
-import meter3 from '../assets/img/meter3.svg'
-import meter4 from '../assets/img/meter1.svg'
-import meter5 from '../assets/img/meter2.svg'
-import colorSharp from  '../assets/img/color-sharp.png'
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
 
-export const Skills=()=>{
-    const responsive={
-        superLargeDesktop:{
-            breakpoint: {max:4000,min:3000},
-            items:5
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
         },
         desktop: {
-            breakpoint: {max: 3000,min: 1024},
+            breakpoint: { max: 3000, min: 1024 },
             items: 3
         },
         tablet: {
-            breakpoint: {max:1024,min:464},
-            items:2
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
         },
         mobile: {
-            breakpoint: { max: 464,min:0},
-            items:1
+            breakpoint: { max: 464, min: 0 },
+            items: 1
         }
-    }
+    };
+
     return (
         <section className="skill" id="skills">
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="skill-bx">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
                             <h2>Skills</h2>
-                            <p>
-                                Full Stack Developer with 3+ years of hands-on experience designing, developing, and implementing applications and solutions using a range
-                                of technologies and PHP programming languages. I seek to leverage broad development experience and hands-on technical expertise in a
-                                challenging role as a Full-stack Developer.
-                            </p>
-                            <Carousel className="skill-slider" responsive={responsive} infinite={true}>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                            <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                                 <div className="item">
-                                    <input type="image" src={meter1} alt="Image"/>
-                                    <h5>Web Developer</h5>
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Web Development</h5>
                                 </div>
                                 <div className="item">
-                                    <input type="image" src={meter2} alt="Image"/>
-                                    <h5>Web Designer</h5>
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Brand Identity</h5>
                                 </div>
                                 <div className="item">
-                                    <input type="image" src={meter3} alt="Image"/>
-                                    <h5>UI/UX Designer</h5>
+                                    <img src={meter3} alt="Image" />
+                                    <h5>Logo Design</h5>
                                 </div>
                                 <div className="item">
-                                    <input type="image" src={meter4} alt="Image"/>
-                                    <h5>PHP</h5>
-                                </div>
-                                <div className="item">
-                                    <input type="image" src={meter5} alt="Image"/>
-                                    <h5>Laravel</h5>
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Web Development</h5>
                                 </div>
                             </Carousel>
                         </div>
-                    </Col>
-                </Row>
-            </Container>
-            <input type="image" img src={colorSharp} className="background-image-left" alt=""/>
+                    </div>
+                </div>
+            </div>
+            <img className="background-image-left" src={colorSharp} alt="Image" />
         </section>
     )
 }
